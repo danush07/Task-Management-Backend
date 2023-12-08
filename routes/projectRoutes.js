c34ed19cef3d3823faef28fe730b955adc018c89
Danush Hari <danushhari2001@gmail.com>
2023-12-08 13:17:28 +0530

    Update projectRoutes.js

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -15,15 +15,15 @@ const {
   } = require('../controllers/projectController');
 const {protect} = require('../middleware/authMiddleware')
 
-router.post('/createproject',protect,createProject)
-router.get('/getprojects',protect,getProjects)
-router.get('/:id',protect,getProjectById)
-router.put('/:id',protect,editProject)
-router.delete('/:id',protect,deleteProject)
-router.post('/addtask',protect,addTask)
-router.delete('/:projectId/task/:taskId',protect,deleteSingleTask)
-router.put('/:projectId/task/:taskId',protect,editSingleTask)
-router.put('/:projectId/task/:taskId/complete',protect,setTaskCompletedStatus)
-router.post('/:projectId/addepicname',protect,addEpicTitle)
+router.post('https://task-management-backend-hpay.onrender.com/createproject',protect,createProject)
+router.get('https://task-management-backend-hpay.onrender.com/getprojects',protect,getProjects)
+router.get('https://task-management-backend-hpay.onrender.com/:id',protect,getProjectById)
+router.put('https://task-management-backend-hpay.onrender.com/:id',protect,editProject)
+router.delete('https://task-management-backend-hpay.onrender.com/:id',protect,deleteProject)
+router.post('https://task-management-backend-hpay.onrender.com/addtask',protect,addTask)
+router.delete('https://task-management-backend-hpay.onrender.com/:projectId/task/:taskId',protect,deleteSingleTask)
+router.put('https://task-management-backend-hpay.onrender.com/:projectId/task/:taskId',protect,editSingleTask)
+router.put('https://task-management-backend-hpay.onrender.com/:projectId/task/:taskId/complete',protect,setTaskCompletedStatus)
+router.post('https://task-management-backend-hpay.onrender.com/:projectId/addepicname',protect,addEpicTitle)
 
 module.exports = router
